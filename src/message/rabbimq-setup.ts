9c93135cf1f856d34d93ff503d0b52f2d4f69ec9
pedro lucas <pedrolucasgomesdeoliveira13@gmail.com>
2024-12-11 09:43:01 -0300

    feat: a

--- src/message/rabbimq-setup.ts
+++ src/message/rabbimq-setup.ts
@@ -1,4 +1,4 @@
-import client, {Channel, Connection} from 'amqplib';
+import client, { Channel, Connection, ConsumeMessage } from 'amqplib';
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -15,6 +15,26 @@ export async function initRabbitMq(): Promise<void> {
 export function sendMessage(payload: any): void {
     const message = JSON.stringify(payload);
     channel.sendToQueue(QUEUE, Buffer.from(message));
+}
+
+export async function consumeMessages(onMessage: (message: any) => void): Promise<void> {
+    if (!channel) {
+        throw new Error("Channel is not initialized. Call initRabbitMq first.");
+    }
+
+    await channel.consume(QUEUE, (msg: ConsumeMessage | null) => {
+        if (msg) {
+            const messageContent = msg.content.toString();
+            try {
+                const parsedMessage = JSON.parse(messageContent);
+                onMessage(parsedMessage);
+                channel.ack(msg); // Acknowledge the message after successful processing
+            } catch (error) {
+                console.error("Failed to process message:", error);
+                channel.nack(msg, false, false); // Reject the message and do not requeue
+            }
+        }
+    });
 }
 
 async function getConnection() {
